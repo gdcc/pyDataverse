c7b2c829132e9e6b942813103ed728f9c673dba2
Stefan Kasberger <mail@stefankasberger.at>
2019-05-02 15:43:37 +0200

    add docstrings and exceptions to utils.py

--- src/pyDataverse/utils.py
+++ src/pyDataverse/utils.py
@@ -5,17 +5,68 @@ import json
 
 
 def json_to_dict(data):
-    """Convert json to a python dict()."""
-    return json.loads(data)
+    """Convert JSON to a dict().
+
+    See more about the json module at
+    https://docs.python.org/3.5/library/json.html
+
+    Parameters
+    ----------
+    data : string
+        `data` as JSON-formatted string.
+
+    Returns
+    -------
+    dict
+        `data` represented as dict().
+
+    """
+    try:
+        return json.loads(data)
+    except Exception as e:
+        raise
 
 
 def dict_to_json(data):
-    """Convert python dict() to json."""
-    return json.dumps(data, ensure_ascii=False, indent=2)
+    """Convert dict() to JSON-formatted string.
+
+    See more about the json module at
+    https://docs.python.org/3.5/library/json.html
+
+    Parameters
+    ----------
+    data : dict
+        `data` represented as dict().
+
+    Returns
+    -------
+    string
+        `data` as JSON-formatted string
+
+    """
+    try:
+        return json.dumps(data, ensure_ascii=False, indent=2)
+    except Exception as e:
+        raise
 
 
 def read_file(filename, mode='r'):
-    """Read in a file."""
+    """Read in data from a file.
+
+    Parameters
+    ----------
+    filename : type
+        Description of parameter `filename`.
+    mode : string
+        Read mode of file. Default is `r`. See more at
+        https://docs.python.org/3.5/library/functions.html#open
+
+    Returns
+    -------
+    string
+        Returns the whole content as string.
+
+    """
     try:
         with open(filename, mode) as f:
             data = f.read()
@@ -24,46 +75,85 @@ def read_file(filename, mode='r'):
         raise e
 
 
-def read_json_file(filename):
-    """Read in a json file."""
-    return json.loads(read_file(filename))
+def write_file(filename, string, mode='w'):
+    """Write data in a file.
 
+    Parameters
+    ----------
+    filename : string
+        Full filename with path of file.
+    string : string
+        String of data to be written.
+    mode : string
+        Read mode of file. Default is `w`. See more at
+        https://docs.python.org/3.5/library/functions.html#open
 
-def read_csv(filename):
-    """Read in a csv file."""
-    with open(filename, newline='') as csvfile:
-        return csv.reader(csvfile, delimiter=',', quotechar='"')
-
-
-def read_datasets_csv(filename):
-    """Read in datasets from a CSV file with doi and dataverse.
-
-    Dataverse must be a alias or an id.
     """
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        next(reader, None)
-        dataset_list = []
-        for row in reader:
-            dataset_list.append({
-                'doi': row[0],
-                'dataverse': row[1]
-            })
-    return dataset_list
+    try:
+        with open(filename, mode) as f:
+            f.write(string)
+    except Exception as e:
+        raise
 
 
-def write_file(filename, string, type='w'):
-    """Write a file."""
-    with open(filename, type) as f:
-        f.write(string)
+def read_file_json(filename):
+    """Read in JSON file.
+
+    See more about the json module at
+    https://docs.python.org/3.5/library/json.html
+
+    Parameters
+    ----------
+    filename : string
+        Full filename with path of file.
+
+    Returns
+    -------
+    dict
+        Data represented as a dict().
+
+    """
+    try:
+        return json.loads(read_file(filename), 'r')
+    except Exception as e:
+        raise
 
 
-def write_json_file(filename, data, type='w'):
-    """Write a json file."""
-    write_file(filename, dict_to_json(data))
+def write_file_json(filename, data, mode='w'):
+    """Write dict() in a JSON file.
+
+    Parameters
+    ----------
+    filename : string
+        Full filename with path of file.
+    data : dict
+        Data to be written in the JSON file.
+    mode : string
+        Write mode of file. Default is `w`. See more at
+        https://docs.python.org/3/library/functions.html#open
+
+    """
+    write_file(filename, dict_to_json(data), mode)
 
 
-def write_datasets_csv(filename):
-    """Write a datasets csv file with doi and related dataverse."""
-    with open(filename, 'w', newline='') as csvfile:
-        return csv.writer(csvfile, delimiter=',', quotechar='"')
+def read_file_csv(filename):
+    """Read in CSV file.
+
+    See more about csv.reader() at https://docs.python.org/3.5/library/csv.html
+
+    Parameters
+    ----------
+    filename : string
+        Full filename with path of file.
+
+    Returns
+    -------
+    reader
+        Reader object, which can be iterated over.
+
+    """
+    try:
+        with open(filename, newline='') as csvfile:
+            return csv.reader(csvfile, delimiter=',', quotechar='"')
+    except Exception as e:
+        raise
