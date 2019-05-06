242da864f8a7729724c3d836186d7b6d9b61c330
Stefan Kasberger <mail@stefankasberger.at>
2019-05-06 15:18:55 +0200

    fix travis issue with curl dependency #3

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     long_description_content_type="text/markdown",
     license='MIT',
     url='https://github.com/AUSSDA/pyDataverse',
-    python_requires='>=3.5',
+    python_requires='>=2.7',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=INSTALL_REQUIREMENTS,
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,32 +5,17 @@ from pyDataverse.api import Api
 
 
 def test_connection_to_api():
-    """Test setup of Dataverse API connection.
-
-    Parameters
-    ----------
-
-
-    Returns
-    -------
-    type
-        Description of returned object.
-
-    """
-    # TODO: pass wrong auth-token, host, api-version
-    host = os.environ.get('HOST')
-    api_auth_token = os.environ.get('API_AUTH_TOKEN')
+    """Test setup of Dataverse API connection."""
+    base_url = 'https://'+os.environ.get('HOST')
+    api_token = os.environ.get('API_AUTH_TOKEN')
     dataverse_api_version = os.environ.get('DATAVERSE_API_VERSION')
     dataverse_version = os.environ.get('DATAVERSE_VERSION')
-    api = Api(host, api_auth_token)
+    api = Api(base_url, api_token)
     assert isinstance(api, Api)
-    assert api.host == host
-    assert api.api_token == api_auth_token
+    assert api.api_token == api_token
     assert api.api_version is not None
     assert isinstance(api.conn_started, datetime)
-    assert api.base_url == 'https://'+host
+    assert api.base_url == 'https://'+os.environ.get('HOST')
     assert api.api_version == dataverse_api_version
-    assert api.native_base_url == 'https://'+host+'/api/v1'
+    assert api.native_api_base_url == api.base_url+'/api/v1'
     assert api.dataverse_version == dataverse_version
-    api = Api(host, api_auth_token, use_https=False)
-    assert api.native_base_url == 'http://'+host+'/api/v1'
