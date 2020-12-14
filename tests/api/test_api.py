6eb8b65834bb5ce94a71a95e7e1146f7ea2efe46
skasberger <mail@stefankasberger.at>
2020-12-14 01:23:36 +0100

    Improve test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -14,30 +14,21 @@ class TestApiConnect(object):
     """Test the NativeApi() class initalization."""
 
     def test_api_connect(self, native_api):
-        """Test successfull connection without api_token."""
-        native_api = NativeApi(test_config["base_url"])
         sleep(test_config["wait_time"])
 
         assert isinstance(native_api, NativeApi)
         assert not native_api.api_token
         assert native_api.api_version == "v1"
-        assert native_api.base_url == test_config["base_url"]
+        assert native_api.base_url == os.getenv("BASE_URL")
         assert native_api.base_url_api_native == "{0}/api/{1}".format(
-            test_config["base_url"], native_api.api_version
+            os.getenv("BASE_URL"), native_api.api_version
         )
 
-    def test_api_connect_base_url_wrong(self, native_api):
+    def test_api_connect_base_url_wrong(self):
         """Test native_api connection with wrong `base_url`."""
         # None
         with pytest.raises(ApiUrlError):
-            base_url = None
-            native_api = NativeApi(base_url)
-            sleep(test_config["wait_time"])
-
-            assert not native_api.api_token
-            assert native_api.api_version == "v1"
-            assert not native_api.base_url
-            assert not native_api.base_url_api_native
+            NativeApi(None)
 
 
 class TestApiRequests(object):
@@ -54,7 +45,8 @@ class TestApiRequests(object):
     def test_get_request(self, native_api):
         """Test successfull `.get_request()` request."""
         # TODO: test params und auth default
-        query_str = test_config["base_url"] + "/api/v1/info/server"
+        base_url = os.getenv("BASE_URL")
+        query_str = base_url + "/api/v1/info/server"
         resp = native_api.get_request(query_str)
         sleep(test_config["wait_time"])
 
