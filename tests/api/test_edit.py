d5d2fcfd401b5059ffe40b4a6e0687ada592f527
Jan Range <range.jan@web.de>
2025-01-15 19:55:54 +0100

    fix missing method

--- tests/api/test_edit.py
+++ tests/api/test_edit.py
@@ -1,13 +1,13 @@
 import json
 import os
-
+import httpx
 from pyDataverse.api import NativeApi
 
 
 class TestEditDatasetMetadata:
     def test_edit_dataset_metadata(self):
         """
-        Test case for uploading a file to a dataset.
+        Test case for editing a dataset's metadata.
 
         This test case performs the following steps:
         1. Creates a dataset using the provided metadata.
@@ -15,7 +15,7 @@ class TestEditDatasetMetadata:
         3. Asserts that the metadata was edited successfully.
 
         Raises:
-            AssertionError: If the file upload fails.
+            AssertionError: If the metadata edit fails.
 
         """
         # Arrange
@@ -50,3 +50,34 @@ class TestEditDatasetMetadata:
             == "New Title"
         ), "Metadata edit failed."
         assert response.status_code == 200, "Metadata edit failed."
+
+    @staticmethod
+    def _create_dataset(
+        BASE_URL: str,
+        API_TOKEN: str,
+        metadata: dict,
+    ):
+        """
+        Create a dataset in the Dataverse.
+
+        Args:
+            BASE_URL (str): The base URL of the Dataverse instance.
+            API_TOKEN (str): The API token for authentication.
+            metadata (dict): The metadata for the dataset.
+
+        Returns:
+            str: The persistent identifier (PID) of the created dataset.
+        """
+        url = f"{BASE_URL}/api/dataverses/root/datasets"
+        response = httpx.post(
+            url=url,
+            json=metadata,
+            headers={
+                "X-Dataverse-key": API_TOKEN,
+                "Content-Type": "application/json",
+            },
+        )
+
+        response.raise_for_status()
+
+        return response.json()["data"]["persistentId"]
