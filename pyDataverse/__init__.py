68cccdc1ef53351b043faf6c8e683a68bb169ba6
Jan Range <30547301+JR-1991@users.noreply.github.com>
2026-02-13 10:38:25 +0100

    Suppress DeprecationWarning, apply nest_asyncio
    Import warnings and nest_asyncio in package init; suppress pkg_resources DeprecationWarning (from a third-party dependency) and call nest_asyncio.apply() to enable nested asyncio event loops (useful in REPLs/notebooks). Also import QueryOptions and core types (Collection, Dataset, Dataverse, File) and expose them via __all__ so they are available from the package namespace.

--- pyDataverse/__init__.py
+++ pyDataverse/__init__.py
@@ -7,6 +7,25 @@ Licensed under the MIT License.
 
 from __future__ import absolute_import
 
+import warnings
+
+import nest_asyncio
+
+from .api.search import QueryOptions
+from .dataverse import Collection, Dataset, Dataverse, File
+
+# Suppress pkg_resources deprecation warnings from fs package
+# These warnings come from a third-party dependency and cannot be fixed
+# in this codebase.
+warnings.filterwarnings(
+    "ignore",
+    message=".*pkg_resources.*",
+    category=DeprecationWarning,
+)
+
+
+nest_asyncio.apply()
+
 __author__ = "Stefan Kasberger"
 __email__ = "stefan.kasberger@univie.ac.at"
 __copyright__ = "Copyright (c) 2019 Stefan Kasberger"
@@ -15,3 +34,11 @@ __version__ = "0.3.4"
 __url__ = "https://github.com/GDCC/pyDataverse"
 __download_url__ = "https://pypi.python.org/pypi/pyDataverse"
 __description__ = "A Python module for Dataverse."
+
+__all__ = [
+    "Dataset",
+    "Dataverse",
+    "Collection",
+    "File",
+    "QueryOptions",
+]
