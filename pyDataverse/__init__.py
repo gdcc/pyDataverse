c29dfc1b4a93fd42c7e9ed44fbcdf7a222ffd036
Jan Range <30547301+JR-1991@users.noreply.github.com>
2026-06-18 11:35:47 +0200

    Use importlib to set __version__; bump to 0.4.0b1
    Replace the hardcoded __version__ with importlib.metadata.version("pyDataverse") so the runtime reports the installed distribution version and raise a RuntimeError if the package isn't installed. Also update the project version to 0.4.0b1 in pyproject.toml to mark a pre-release.

--- pyDataverse/__init__.py
+++ pyDataverse/__init__.py
@@ -8,6 +8,7 @@ Licensed under the MIT License.
 from __future__ import absolute_import
 
 import warnings
+from importlib.metadata import PackageNotFoundError, version
 
 import nest_asyncio
 
@@ -30,10 +31,15 @@ __author__ = "Stefan Kasberger"
 __email__ = "stefan.kasberger@univie.ac.at"
 __copyright__ = "Copyright (c) 2019 Stefan Kasberger"
 __license__ = "MIT License"
-__version__ = "0.3.5"
+
 __url__ = "https://github.com/GDCC/pyDataverse"
 __download_url__ = "https://pypi.python.org/pypi/pyDataverse"
 __description__ = "A Python module for Dataverse."
+
+try:
+    __version__ = version("pyDataverse")
+except PackageNotFoundError:
+    raise RuntimeError("pyDataverse is not installed")
 
 __all__ = [
     "Dataset",
